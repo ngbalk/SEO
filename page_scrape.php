1b98c6624784230d95643577cb59221b5b7be5ff
Nick Balkissoon <nick@frontcoding.com>
2013-10-24 13:52:21 +0800

    No major changes, still working on database interaction

--- page_scrape.php
+++ page_scrape.php
@@ -1,11 +1,20 @@
 <?php
+/*
+Page Scraper
+Author: Nick Balkissoon
+@FrontCoding
+Email: ngbalk@gmail.com
+
+TODO: 
+CREATE method for getting doctype, whois data, file size, file type.  Currently we are inserting into the database using static attributes
+for sake of debugging.
+*/
 require_once "ultimate-web-scraper/support/http.php";
 require_once "ultimate-web-scraper/support/web_browser.php";
 require_once "ultimate-web-scraper/support/simple_html_dom.php";
 $myPage = new Page("http://www.frontcoding.com");
-echo $myPage->title->plaintext . " " . print_r(arsort($myPage->myWords));
-
-class Page{
+echo $myPage->title->plaintext . " " . var_dump($myPage->myWords) . var_dump($myPage->myTags) . $myPage->html->plaintext;
+class Page {
 	public $html; //store as simple_html_dom
 	public $url; //should be a string
 	public $title;
@@ -14,11 +23,11 @@ class Page{
 	public $links_count_out; //no longer needed
 	public $timestamp;
 	public $websiteid; //not sure how this will be determined
-	public $tagsarray;
+	public $myTags;
 	public $myWords;
 
 	public function __construct($url){
-		$this->tagsarray = array();
+		
 		$this->url = $url;
 		$this->html = new simple_html_dom();
 		$web = new WebBrowser();
@@ -32,6 +41,7 @@ class Page{
 	  			$this->get_doctype();
 	  			$this->find_html_tags();
 	  			$this->find_all_words();
+	  			$this->init_save_page();
 	  		}
 
 	}
@@ -78,13 +88,19 @@ class Page{
 		}
 
 	public function find_html_tags(){
-	$htmltags = array("head","meta[content]","title","body","div","a[href]","span","bold","p",
+		$this->myTags = array();
+		$htmltags = array("head","meta[content]","title","body","div","a[href]","span","bold","p",
 				"header","ul","ol","li","table","tr","td","h1","h2","h3","h4","h5", "h6", "footer",
 				"img[src]","img[alt]","menu","strong","a");
 		foreach ($htmltags as $tag) {
-		$tagcontent = $this->html->find($tag);
-		array_push($this->tagsarray, $tagcontent);
-	}
+			$concat = "";
+			$tagcontent = $this->html->find($tag);
+			foreach ($tagcontent as $it) {
+				$concat.= " " . $it->plaintext;
+			}
+			$this->myTags[$tag] = $concat;
+			
+		}
 	}
 	public function find_all_words(){
 		$tagstocheck = array("p","h1","h2","h3","h4","h5","h6","title","li","td","alt","a");
@@ -101,28 +117,65 @@ class Page{
 					}
 				}
 			}
+			arsort($wordfreqs);
 			$this->myWords=$wordfreqs;
 		}
-		public function get_file_size(){
+	public function get_file_size(){
 			/*
 			TODO implement a method that finds the size of the file @url
 			*/
 		}
-		public function get_whois(){
+	public function get_whois(){
 			/*
 			TODO implement method to scrape whois data
 			*/
 		}
+//fuckkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+	public function init_save_page(){
+		$connect = mysqli_connect("localhost","root","root", "seo");
+		if(!$connect){
+			die("Error connecting" . mysqli_error());
+		}
+		else{
+			echo "succesful connection";
+		}
+		$htmltoinsert = $this->html->plaintext;
+		$raw_data_insert= mysqli_query($connect, "INSERT INTO raw_data (url, data_type, size, scraper_data) VALUES ('$this->url', 'html', 10, 'fieiuhaiehfiewhf')");
+		if(!$raw_data_insert){
+			die("raw data insert error: " . mysqli_connect_error());
+
+		}
+		$raw_data_id = mysqli_query($connect, "SELECT last_insert_id()");
+		$webpages_insert=mysqli_query($connect, "INSERT INTO webpages (tag_title, doctype, raw_data_id) VALUES ('$this->title', 'html', '$raw_data_id')");
+		if(!$webpages_insert){
+			die("webpages insert error: " . mysqli_connect_error());
+		}
+		$parent_id = mysqli_query($connect, "SELECT last_insert_id()");
+		//Do word insert into db
+		foreach ($this->myWords as $word => $frequency) {
+			
+			$toinsert = mysqli_query($connect, "INSERT INTO words (parent_id, word, frequency, parent_type) VALUES ('$parent_id', '$word', '$frequency', 'html')");
+			if(!$toinsert){
+				die("word insert error: " .  mysqli_connect_error());
+			}
+		}
+		//Do tag insert into db
+		foreach ($this->myTags as $tag => $content) {
+		
+			$toinsert = mysqli_query($connect, "INSERT INTO tags (parent_id, tag, parent_type, content) VALUES ('$parent_id', '$tag', 'html', '$content')");
+			if(!$toinsert){
+				die("word insert error: " .  mysqli_connect_error());
+			}
+		}
+		echo "_____DATABASE SAVE COMPLETE______";
+	}
 
 	
 
 
 
 }
-abstract class Update{
-	abstract public function save_all();
-	abstract public function update_only();
-}
+
 
 
 
