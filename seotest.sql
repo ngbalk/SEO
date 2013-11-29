7cf162f78daf3da1a9adbb7ac8f6ceb672e4d034
Nick Balkissoon <nick@frontcoding.com>
2013-11-29 12:48:16 +0800

    With seotest.sql to create databases

---
+++ seotest.sql
@@ -0,0 +1,2 @@
+<br />
+<b>Fatal error</b>:  Allowed memory size of 134217728 bytes exhausted (tried to allocate 47796224 bytes) in <b>C:\xampp\phpMyAdmin\libraries\Response.class.php</b> on line <b>273</b><br />
